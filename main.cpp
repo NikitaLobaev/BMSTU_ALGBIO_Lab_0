8d155124d25424d514abf58e9918c9ae2f84d1d7
Nikita Lobaev <lobaevni@yandex-team.ru>
2021-09-16 18:15:23 +0300

    Implement the algorithm

--- main.cpp
+++ main.cpp
@@ -1,95 +1,85 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <queue>
 #include <vector>
 
 typedef std::pair<std::string, std::string> DNA; //<dna_id, dna_string>
 
 void read_dnas(const std::string&, DNA&, std::vector<DNA>&);
 
-std::string find_lcs(const std::string&, const std::string&);
-
-bool compare_dnas(const DNA&, const DNA&, const DNA&);
+size_t find_lcs_length(const std::string&, const std::string&);
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cerr << "Usage: lab1 <filename>" << std::endl;
+    if (argc != 3) {
+        std::cerr << "Usage: lab1 <filename> <count>" << std::endl;
         return 1;
     }
+
+    const std::string filename = argv[1];
+    const size_t count = std::stoul(argv[2]);
 
     DNA main_dna;
     std::vector<DNA> dnas;
 
-    read_dnas(argv[1], main_dna, dnas);
+    read_dnas(filename, main_dna, dnas);
 
-    std::cout << "Starting sort... (it may require some minutes)" << std::endl;
+    std::cout << "Starting sort... (it may require some time)" << std::endl;
 
-    std::sort(dnas.begin(), dnas.end(), [main_dna](const DNA &dna1, const DNA &dna2) {
-        return compare_dnas(main_dna, dna1, dna2);
-    });
+    std::priority_queue<std::pair<size_t, DNA>> pq;
+    for (size_t index = std::min(count, dnas.size()); index > 0; index--) {
+        pq.emplace(find_lcs_length(main_dna.second, dnas[index - 1].second), dnas[index - 1]);
+    }
+    for (size_t index = count; index < dnas.size(); index++) {
+        pq.emplace(find_lcs_length(main_dna.second, dnas[index].second), dnas[index]);
+        pq.pop();
+    }
+
+    std::vector<DNA> top_dnas;
+    while (!pq.empty()) {
+        const DNA dna = pq.top().second;
+        top_dnas.push_back(dna);
+        pq.pop();
+    }
+    top_dnas.shrink_to_fit();
 
     std::cout << std::endl << "<DNA ID>: <DNA>" << std::endl << std::endl;
     std::cout << main_dna.first << ": " << main_dna.second << std::endl << std::endl;
-    for (const DNA &dna : dnas) {
-        std::cout << dna.first << ": " << dna.second << std::endl;
+    for (auto it = top_dnas.rbegin(); it != top_dnas.rend(); it++) {
+        std::cout << it->first << ": " << it->second << std::endl;
     }
-
     return 0;
 }
 
-enum Direction {
-    UP, LEFT, UP_LEFT
-};
-
-std::string count_lcs(const std::string&, const std::vector<std::vector<Direction>>&, size_t, size_t);
-
-std::string find_lcs(const std::string &x, const std::string &y) {
+size_t find_lcs_length(const std::string &x, const std::string &y) {
     if (x.empty() || y.empty()) {
-        return "";
+        return 0;
     }
 
     const size_t x_length = x.length();
     const size_t y_length = y.length();
 
-    std::vector<std::vector<Direction>> directions(x_length + 1, std::vector<Direction>(y_length + 1));
     std::vector<std::vector<size_t>> dp(x_length + 1, std::vector<size_t>(y_length + 1));
 
     for (size_t i = 1; i <= x_length; i++) {
         for (size_t j = 1; j <= y_length; j++) {
             if (x[i - 1] == y[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
-                directions[i][j] = UP_LEFT;
             } else if (dp[i - 1][j] >= dp[i][j - 1]) {
                 dp[i][j] = dp[i - 1][j];
-                directions[i][j] = UP;
             } else {
                 dp[i][j] = dp[i][j - 1];
-                directions[i][j] = LEFT;
             }
         }
     }
 
-    return count_lcs(x, directions, x_length, y_length);
-}
-
-std::string count_lcs(const std::string &string, const std::vector<std::vector<Direction>> &directions,
-                      size_t i, size_t j) {
-    if (i == 0 || j == 0) {
-        return "";
-    }
-
-    if (directions[i][j] == UP_LEFT) {
-        return count_lcs(string, directions, i - 1, j - 1) + string[i - 1];
-    } else if (directions[i][j] == UP) {
-        return count_lcs(string, directions, i - 1, j);
-    } else {
-        return count_lcs(string, directions, i, j - 1);
-    }
+    return dp[x_length][y_length];
 }
 
 void read_dnas(const std::string &filename, DNA &main_dna, std::vector<DNA> &dnas) {
     std::ifstream ifstream(filename);
-    for (std::string cur_line, cur_dna_buf; std::getline(ifstream, cur_line);) {
+    std::string cur_line, cur_dna_buf;
+    while (std::getline(ifstream, cur_line)) {
         if (cur_line.empty()) {
             continue;
         }
@@ -113,10 +103,13 @@ void read_dnas(const std::string &filename, DNA &main_dna, std::vector<DNA> &dna
             cur_dna_buf += cur_line;
         }
     }
-}
 
-bool compare_dnas(const DNA &main_dna, const DNA &dna1, const DNA &dna2) {
-    const std::string max1 = find_lcs(main_dna.second, dna1.second);
-    const std::string max2 = find_lcs(main_dna.second, dna2.second);
-    return max1.length() > max2.length(); //greater comparator
+    if (!cur_dna_buf.empty()) {
+        if (main_dna.first.empty()) {
+            main_dna = {dnas.back().first, cur_dna_buf};
+            dnas.pop_back();
+        } else {
+            dnas.back().second = cur_dna_buf;
+        }
+    }
 }
